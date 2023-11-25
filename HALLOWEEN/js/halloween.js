c1bd7d89dcfaf5400b878fa2a0b9049c37e1d409
Cristian Rodríguez <crh161205@gmail.com>
2023-11-25 14:01:05 +0100

    Trabajo de halloween con todo añadido

---
+++ HALLOWEEN/js/halloween.js
@@ -0,0 +1,21 @@
+/**------------------------------------------------------------------------
+ * ?                                ABOUT
+ * @author         :  
+ * @email          :  
+ * @repo           :  
+ * @createdOn      :  
+ * @description    :  
+ *------------------------------------------------------------------------**/
+function show(element) {
+    document.getElementById(element).style.visibility="visible";
+}
+
+function hide(element) {
+    document.getElementById(element).style.visibility="hidden";
+}
+function sound(element) {
+    document.getElementById(element).play();
+}
+function silence (element) {
+    document.getElementById(element).pause();
+}
